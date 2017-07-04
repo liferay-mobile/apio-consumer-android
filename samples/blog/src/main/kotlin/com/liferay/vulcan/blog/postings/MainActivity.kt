a13fc3e0cb23e7fe42ede17fac3a12b9d51de154
Alejandro Hernández <info@alejandrohdezma.com>
2017-07-04 17:47:27 +0200

    VULCAN-27 Extract `SecondActivity` into its own file

--- samples/blog/src/main/kotlin/com/liferay/vulcan/blog/postings/MainActivity.kt
+++ samples/blog/src/main/kotlin/com/liferay/vulcan/blog/postings/MainActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.TextView
-import com.github.kittinunf.result.success
-import com.liferay.vulcan.consumer.fetch
 import com.liferay.vulcan.consumer.model.Relation
 import com.liferay.vulcan.consumer.model.Thing
 import com.liferay.vulcan.consumer.model.get
@@ -13,8 +11,6 @@ import com.liferay.vulcan.consumer.screens.ScreenletEvents
 import com.liferay.vulcan.consumer.screens.ThingScreenlet
 import com.liferay.vulcan.consumer.screens.views.CollectionView
 import com.liferay.vulcan.consumer.screens.views.ThingView
-import okhttp3.HttpUrl
-import org.jetbrains.anko.longToast
 import org.jetbrains.anko.startActivity
 
 class MainActivity : AppCompatActivity(), ScreenletEvents {
@@ -27,7 +23,7 @@ class MainActivity : AppCompatActivity(), ScreenletEvents {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val id = "http://192.168.50.222:8080/o/api/group/20149/p/blogs"
+        val id = "http://192.168.0.156:8080/o/api/group/20143/p/blogs"
 
         thingScreenlet.load(id) {
             (it.viewModel as CollectionView).customLayout = Pair(R.layout.blog_posting_row,
@@ -57,50 +53,5 @@ class MainActivity : AppCompatActivity(), ScreenletEvents {
 
         startActivity<SecondActivity>("id" to thing.id)
     }
-}
 
-class SecondActivity : AppCompatActivity() {
-
-    val thingScreenlet by lazy {
-        findViewById(R.id.thing_screenlet) as ThingScreenlet
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
-
-        val id = intent.getStringExtra("id")
-
-        thingScreenlet.load(id)
-
-        //TODO move to screenlet
-        val knowMore = findViewById(R.id.know_more)
-        val httpURL = HttpUrl.parse(id)
-        fetch(httpURL!!, embedded = listOf("creator")) {
-            it.fold(
-                success = { thing ->
-                    (findViewById(R.id.headline) as TextView).apply {
-                        text = thing["headline"] as String
-                    }
-
-                    (findViewById(R.id.creator) as TextView).apply {
-                        val author = thing.attributes["author"] as Relation
-                        text = author.id
-
-                        knowMore.setOnClickListener {
-                            fetch(HttpUrl.parse(author.id)!!) {
-                                it.success {
-                                    text = it["name"] as String
-                                }
-                            }
-                        }
-                    }
-                },
-                failure = {
-                    longToast(it.message!!)
-                }
-            )
-
-        }
-    }
 }
---
+++ samples/blog/src/main/kotlin/com/liferay/vulcan/blog/postings/SecondActivity.kt
@@ -0,0 +1,59 @@
+package com.liferay.vulcan.blog.postings
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
+import com.github.kittinunf.result.success
+import com.liferay.vulcan.consumer.fetch
+import com.liferay.vulcan.consumer.model.Relation
+import com.liferay.vulcan.consumer.model.get
+import com.liferay.vulcan.consumer.screens.ThingScreenlet
+import okhttp3.HttpUrl
+import org.jetbrains.anko.longToast
+
+class SecondActivity : AppCompatActivity() {
+
+    val thingScreenlet by lazy {
+        findViewById(R.id.thing_screenlet) as ThingScreenlet
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        val id = intent.getStringExtra("id")
+
+        thingScreenlet.load(id)
+
+        //TODO move to screenlet
+        val knowMore = findViewById(R.id.know_more)
+        val httpURL = HttpUrl.parse(id)
+        fetch(httpURL!!, embedded = listOf("creator")) {
+            it.fold(
+                success = { thing ->
+                    (findViewById(R.id.headline) as TextView).apply {
+                        text = thing["headline"] as String
+                    }
+
+                    (findViewById(R.id.creator) as TextView).apply {
+                        val author = thing.attributes["author"] as Relation
+                        text = author.id
+
+                        knowMore.setOnClickListener {
+                            fetch(HttpUrl.parse(author.id)!!) {
+                                it.success {
+                                    text = it["name"] as String
+                                }
+                            }
+                        }
+                    }
+                },
+                failure = {
+                    longToast(it.message!!)
+                }
+            )
+
+        }
+    }
+
+}
