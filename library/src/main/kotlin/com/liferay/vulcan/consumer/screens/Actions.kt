4fbd15c1ce9220d4602dd92f38e364edb760c94f
Alejandro Hernández <info@alejandrohdezma.com>
2017-07-10 15:34:53 +0200

    VULCAN-41 Create ViewScenario enum
    This will be used to add semantics to each layout

--- library/src/main/kotlin/com/liferay/vulcan/consumer/screens/Actions.kt
+++ library/src/main/kotlin/com/liferay/vulcan/consumer/screens/Actions.kt
@@ -6,3 +6,7 @@ import com.liferay.vulcan.consumer.model.Thing
 sealed class Action<T>
 
 class ClickAction(val view: View, val thing: Thing) : Action<View.OnClickListener>()
+
+enum class ViewScenario {
+    ROW, DETAIL
+}
