4d4349adcfcb624e23604417f9796c4d811b320d
Alejandro Hernández <info@alejandrohdezma.com>
2017-07-11 20:09:32 +0200

    VULCAN-41 VULCAN-32 Transform BlogPostingViewHolder into BlogPostingRowView

--- library/src/main/kotlin/com/liferay/vulcan/consumer/screens/views/row/BlogPostingRowView.kt
+++ library/src/main/kotlin/com/liferay/vulcan/consumer/screens/views/row/BlogPostingRowView.kt
@@ -1,21 +1,20 @@
-package com.liferay.vulcan.consumer.screens.viewholder
+package com.liferay.vulcan.consumer.screens.views.row
 
-import android.view.View
+import android.content.Context
+import android.util.AttributeSet
 import android.widget.TextView
 import com.liferay.vulcan.consumer.R
 import com.liferay.vulcan.consumer.delegates.bindNonNull
 import com.liferay.vulcan.consumer.delegates.converter
 import com.liferay.vulcan.consumer.model.BlogPosting
 import com.liferay.vulcan.consumer.model.Thing
-import com.liferay.vulcan.consumer.screens.adapter.ThingViewHolder
+import com.liferay.vulcan.consumer.screens.views.BaseView
 
-class BlogPostingViewHolder(itemView: View, listener: Listener) : ThingViewHolder(itemView, listener) {
+class BlogPostingRowView(context: Context, attrs: AttributeSet) : BaseView(context, attrs) {
     val headline by bindNonNull<TextView>(R.id.headline)
     val creator by bindNonNull<TextView>(R.id.creator)
 
     override var thing: Thing? by converter<BlogPosting> {
-        super.thing = this.thing
-
         headline.text = it.headline
         creator.text = it.creator?.id
     }
