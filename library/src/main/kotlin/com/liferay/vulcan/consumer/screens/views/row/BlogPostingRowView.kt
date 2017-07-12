19d568a0c659b7cb665b3b70b4d5efb36df3fe29
Alejandro Hernández <info@alejandrohdezma.com>
2017-07-12 11:52:28 +0200

    VULCAN-41 VULCAN-26 Transform creator's TextView into ThingScreenlet with "portrait" scenario

--- library/src/main/kotlin/com/liferay/vulcan/consumer/screens/views/row/BlogPostingRowView.kt
+++ library/src/main/kotlin/com/liferay/vulcan/consumer/screens/views/row/BlogPostingRowView.kt
@@ -8,14 +8,18 @@ import com.liferay.vulcan.consumer.delegates.bindNonNull
 import com.liferay.vulcan.consumer.delegates.converter
 import com.liferay.vulcan.consumer.model.BlogPosting
 import com.liferay.vulcan.consumer.model.Thing
+import com.liferay.vulcan.consumer.screens.ThingScreenlet
 import com.liferay.vulcan.consumer.screens.views.BaseView
 
 class BlogPostingRowView(context: Context, attrs: AttributeSet) : BaseView(context, attrs) {
     val headline by bindNonNull<TextView>(R.id.headline)
-    val creator by bindNonNull<TextView>(R.id.creator)
+    val creator by bindNonNull<ThingScreenlet>(R.id.creator_avatar)
 
     override var thing: Thing? by converter<BlogPosting> {
         headline.text = it.headline
-        creator.text = it.creator?.id
+
+        it.creator?.also {
+            creator.load(it.id)
+        }
     }
 }
