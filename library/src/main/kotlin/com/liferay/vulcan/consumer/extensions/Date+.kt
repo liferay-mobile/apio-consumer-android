a45d7af9cd6c5673f5ef3eafef492ef6c464c83b
Alejandro Hernández <info@alejandrohdezma.com>
2017-07-17 09:18:50 +0200

    VULCAN-30 Add locale as default parameter in date extension methods

--- library/src/main/kotlin/com/liferay/vulcan/consumer/extensions/Date+.kt
+++ library/src/main/kotlin/com/liferay/vulcan/consumer/extensions/Date+.kt
@@ -4,12 +4,12 @@ import java.text.DateFormat
 import java.util.Date
 import java.util.Locale
 
-fun Date.shortFormat() = format(DateFormat.getDateInstance(DateFormat.SHORT, Locale.US))
+fun Date.shortFormat(locale: Locale = Locale.US) = format(DateFormat.getDateInstance(DateFormat.SHORT, locale))
 
-fun Date.mediumFormat() = format(DateFormat.getDateInstance(DateFormat.MEDIUM, Locale.US))
+fun Date.mediumFormat(locale: Locale = Locale.US) = format(DateFormat.getDateInstance(DateFormat.MEDIUM, locale))
 
-fun Date.longFormat() = format(DateFormat.getDateInstance(DateFormat.LONG, Locale.US))
+fun Date.longFormat(locale: Locale = Locale.US) = format(DateFormat.getDateInstance(DateFormat.LONG, locale))
 
-fun Date.fullFormat() = format(DateFormat.getDateInstance(DateFormat.FULL, Locale.US))
+fun Date.fullFormat(locale: Locale = Locale.US) = format(DateFormat.getDateInstance(DateFormat.FULL, locale))
 
 fun Date.format(format: DateFormat): String = format.format(this)
