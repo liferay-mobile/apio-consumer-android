49387093051602aebcbf60c97216809278af26b1
Alejandro Hernández <info@alejandrohdezma.com>
2017-07-18 10:29:59 +0200

    VULCAN-41 Move md5 methods to PortraitView

--- library/src/main/kotlin/com/liferay/vulcan/consumer/extensions/String+.kt
+++ library/src/main/kotlin/com/liferay/vulcan/consumer/extensions/String+.kt
@@ -1,7 +1,5 @@
 package com.liferay.vulcan.consumer.extensions
 
-import java.nio.charset.Charset
-import java.security.MessageDigest
 import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.Date
@@ -9,14 +7,3 @@ import java.util.Locale
 
 fun String.asDate(format: SimpleDateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.US)): Date? =
     try { format.parse(this) } catch (parseException: ParseException) { null }
-
-fun String.md5(): String {
-    return MessageDigest.getInstance("MD5").run {
-        update(this@md5.toByteArray(Charset.defaultCharset()))
-        digest()
-    }.let {
-        it.fold(StringBuilder()) { builder, byte ->
-            builder.append(byte.toHexString())
-        }
-    }.toString()
-}
--- library/src/main/kotlin/com/liferay/vulcan/consumer/screens/views/custom/PersonPortraitView.kt
+++ library/src/main/kotlin/com/liferay/vulcan/consumer/screens/views/custom/PersonPortraitView.kt
@@ -6,13 +6,14 @@ import android.widget.ImageView
 import com.liferay.vulcan.consumer.R
 import com.liferay.vulcan.consumer.delegates.bindNonNull
 import com.liferay.vulcan.consumer.delegates.converter
-import com.liferay.vulcan.consumer.extensions.md5
 import com.liferay.vulcan.consumer.model.Person
 import com.liferay.vulcan.consumer.model.Thing
 import com.liferay.vulcan.consumer.screens.ThingScreenlet
 import com.liferay.vulcan.consumer.screens.views.BaseView
 import com.squareup.picasso.Picasso
 import de.hdodenhof.circleimageview.CircleImageView
+import java.nio.charset.Charset
+import java.security.MessageDigest
 
 class PersonPortraitView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : BaseView,
@@ -26,10 +27,27 @@ class PersonPortraitView @JvmOverloads constructor(
         it.email
             ?.trim()
             ?.toLowerCase()
-            ?.let(String::md5)
+            ?.md5()
             ?.let { "https://www.gravatar.com/avatar/$it?d=retro" }
             ?.also {
                 Picasso.with(context).load(it).into(imageView)
             }
     }
+
+    private fun String.md5(): String {
+        val CHARS = arrayOf('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f')
+
+        return MessageDigest.getInstance("MD5").run {
+            update(this@md5.toByteArray(Charset.defaultCharset()))
+            digest()
+        }.let {
+            it.fold(StringBuilder()) { builder, byte ->
+                val i = byte.toInt()
+                val char2 = CHARS[i and 0x0f]
+                val char1 = CHARS[i shr 4 and 0x0f]
+                builder.append("$char1$char2")
+            }
+        }.toString()
+    }
+
 }
