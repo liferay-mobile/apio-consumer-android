d89f97086bf2c7777a572b52541487cc60f33b1c
Paulo Cruz <paulo.cruz@liferay.com>
2018-09-22 08:12:10 -0300

    LPS-85528 Create a RequestUtil to extract util functions from ApioConsumer

---
+++ apio-consumer/src/main/kotlin/com/liferay/apio/consumer/util/RequestUtil.kt
@@ -0,0 +1,121 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.apio.consumer.util
+
+import com.google.gson.Gson
+import com.liferay.apio.consumer.exception.ApioException
+import com.liferay.apio.consumer.exception.ThingNotFoundException
+import com.liferay.apio.consumer.model.Thing
+import com.liferay.apio.consumer.model.get
+import okhttp3.*
+import java.io.ByteArrayOutputStream
+import java.io.InputStream
+import java.lang.Exception
+import java.net.URLConnection
+
+/**
+ * @author Paulo Cruz
+ */
+class RequestUtil {
+
+    companion object {
+        fun createRequest(httpUrl: HttpUrl?): Request {
+            return Request.Builder()
+                .url(httpUrl!!)
+                .addHeader("Accept", "application/ld+json")
+                .build()
+        }
+
+        fun createRequest(httpUrl: HttpUrl?, method: String, requestBody: RequestBody?): Request {
+            return createRequest(httpUrl)
+                .newBuilder()
+                .method(method, requestBody)
+                .build()
+        }
+
+        fun createUrl(url: HttpUrl, fields: Map<String, List<String>>, embedded: List<String>): HttpUrl {
+            return url.newBuilder()
+                .apply {
+                    fields.forEach { (type, values) ->
+                        val types = values.joinToString(separator = ",")
+
+                        this.addQueryParameter("fields[$type]", types)
+                    }
+                }
+                .addQueryParameter("embedded", embedded.joinToString(","))
+                .build()
+        }
+
+        fun getRequestBody(attributes: Map<String, Any>): RequestBody {
+            if (attributes.values.none { it is InputStream }) {
+                val json = Gson().toJson(attributes)
+
+                return RequestBody.create(MediaType.parse("application/json; charset=utf-8"), json)
+            }
+            else {
+                val builder = MultipartBody.Builder().setType(MultipartBody.FORM)
+
+                for ((key, value) in attributes) {
+                    if (value is InputStream) {
+                        val byteArray = getByteArrayFromInputStream(value)
+                        val contentType = getContentType(attributes, value)
+                        val body = RequestBody.create(MediaType.parse(contentType), byteArray)
+
+                        builder.addFormDataPart(key, key, body)
+                    }
+                    else {
+                        builder.addFormDataPart(key, value as String)
+                    }
+                }
+
+                return builder.build()
+            }
+        }
+
+        fun getResponseException(thing: Thing, response: Response): Exception {
+            return thing.let {
+                it["title"] as? String
+            }?.let {
+                ApioException(it)
+            } ?: run {
+                if(response.message().isNotEmpty()) {
+                    ApioException(response.message())
+                } else {
+                    ThingNotFoundException()
+                }
+            }
+        }
+
+        private fun getByteArrayFromInputStream(inputStream: InputStream): ByteArray {
+            val byteBuffer = ByteArrayOutputStream()
+
+            inputStream.use { input ->
+                byteBuffer.use {  output ->
+                    input.copyTo(output)
+                }
+            }
+
+            return byteBuffer.toByteArray()
+        }
+
+        private fun getContentType(attributes: Map<String, Any>, inputStream: InputStream): String {
+            val name = attributes["name"] as? String
+
+            return URLConnection.guessContentTypeFromStream(inputStream)
+                ?: URLConnection.guessContentTypeFromName(name)
+                ?: "application/*"
+        }
+    }
+}
